b0e5f0fa6c8ef29ee124e22922f32939545c81b1
Michael Wilkinson <miwilkin@syr.edu>
2016-02-29 17:49:17 +0100

    GangaOutput.py now prints the job and subjob it's on

--- GangaOutput.py
+++ GangaOutput.py
@@ -1,10 +1,14 @@
 import os, sys
 jid = sys.argv[1]
+# jid = 317
 j = jobs(jid)
 base_path = '/tmp/mwilkins/'
+s = 'Job ID = ' + repr(jid)
+print s
 
 for sj in j.subjobs:
    if sj.status != 'completed': continue
+   s = 'On subjob ' + repr(sj)
    for f in sj.outputfiles.get(DiracFile):
       #if (sj.id == 21 or sj.id == 57 or sj.id == 61 or sj.id == 48 or sj.id ==386):
       whole_path = os.path.join(base_path, sj.fqid)
