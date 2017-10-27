db5076ddc8e8a2448d435315484b63f3f7b68cbd
Michael Wilkinson <miwilkin@syr.edu>
2017-10-27 11:10:44 +0200

    bfch: added documentation to make it easier to understand the different ways to add cuts

--- python/bfch.py
+++ python/bfch.py
@@ -10,22 +10,22 @@ class bfch: #abstract base class for branch and fch classes
                 raise TypeError('passed a '+c.__class__.__name__+' as "c" instead of a list.')
     def add_cut(self,*args,**kwargs):
         if isinstance(args[0],cut):
-            if len(args)==1 and len(kwargs)==0:
-                self.c.append(*args)
-            elif len(args)==2 and isinstance(args[1],str):
-                self.add_cut(args[0].cut,args[1])
-            elif all(isinstance(x, cut) for x in args):
-                self.c.extend(args)
+            if len(args)==1 and len(kwargs)==0: # if it's just one cut
+                self.c.append(*args) # just add it
+            elif len(args)==2 and isinstance(args[1],str): # if it's a cut and a string
+                self.add_cut(args[0].cut,args[1]) # add the cut and rename it
+            elif all(isinstance(x, cut) for x in args): # if it's a bunch of cuts
+                self.c.extend(args) # just add them
             else:
                 raise TypeError
-        elif all(isinstance(x, list) for x in args):
+        elif all(isinstance(x, list) for x in args): # if it's a list (or lists)
             for ar in args:
-                if all(isinstance(x, cut) for x in ar):
+                if all(isinstance(x, cut) for x in ar): # of cuts, add them
                     self.c.extend(ar)
-                elif all(isinstance(x, (str,TCut)) for x in ar):
+                elif all(isinstance(x, (str,TCut)) for x in ar): # of strings or TCuts
                     for ct in ar:
-                        self.add_cut(ct)
+                        self.add_cut(ct) # add_cut each of them
                 else:
                     raise TypeError
         else:
-            self.c.append(cut(*args,**kwargs))
+            self.c.append(cut(*args,**kwargs)) # create new cut, append it
