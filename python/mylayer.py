9ce78d934f9acce471ea7071971c1844bdd149de
Michael Wilkinson <miwilkin@syr.edu>
2017-11-08 13:58:05 +0100

    Revert "some changes to how mylayer works"
    This reverts commit bc1b8ed022a38b35390d6e93ab63405b11e35b4c.

--- python/mylayer.py
+++ python/mylayer.py
@@ -1,14 +1,9 @@
 class mylayer:
-    def __init__(self,element,name=None):#,compared=False):
-        if not isinstance(element,list):
-            raise TypeError('element is a '+type(element)+', not a list')
-        if not (element or name):
-            raise IOError('cannot pass empty list without passing a name')
+    def __init__(self,element):
+        if len(element) == 0:
+            raise IndexError('can only create layers from not-empty lists')
+        self.name=element[0].__class__.__name__ #this is the type of layer
         self.element=element #element is a list of objects
-        self.name = name
-        if not self.name:
-            self.name=element[0].__class__.__name__ #this is the type of layer
-        self.compared = False #indicates whether this layer is to be drawn on a canvas with other layers
         self.nL= len(self.element)
         self.Li = 0 #index indicating which element/comparison of the layer we are on should be iterated in code
         self.nLec=0 #should be set = element.size() or comparison.size()
@@ -16,6 +11,7 @@ class mylayer:
         self.plLx = 1 #products of things in lower levels that are not branches or cuts
         self.plLec = 1 #products of things in lower levels using nLec instead of nL
         self.plLecx = 1
+        self.compared = False #indicates whether this layer is to be drawn on a canvas with other layers
         self.comparison = {}
     def add_element(self,temp):
         self.element.append(temp)
