dce6e3594d6b642308b93f9ac3c77768159c3f07
Michael Wilkinson <miwilkin@syr.edu>
2018-06-21 09:31:48 -0400

    consolidated calculators into fxns

--- python/calculators.py
+++
@@ -1,9 +0,0 @@
-def shim_thickness(start, stop, subtract):
-    ''' sums from start to stop (inclusive) and subtracts subtract
-    start, stop, subtract, and output given in thousandths of an inch (no fractions)
-    '''
-    out_value = 0
-    for i in xrange(start, stop + 1):
-        out_value += i
-    out_value -= subtract
-    return out_value
--- python/fxns.py
+++ python/fxns.py
@@ -45,3 +45,14 @@ def replE(ps, ms, *args):
         outstr += ', 2)'
     outstr += ')'
     return outstr
+
+
+def shim_thickness(start, stop, subtract):
+    ''' sums from start to stop (inclusive) and subtracts subtract
+    start, stop, subtract, and output given in thousandths of an inch (no fractions)
+    '''
+    out_value = 0
+    for i in xrange(start, stop + 1):
+        out_value += i
+    out_value -= subtract
+    return out_value
