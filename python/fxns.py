0f3d208b29098fb66e961f5b58b7362604c8e4d3
Michael Wilkinson <miwilkin@syr.edu>
2018-06-22 16:21:30 -0400

    check stop > start in shim_thickness

--- python/fxns.py
+++ python/fxns.py
@@ -51,6 +51,7 @@ def shim_thickness(start, stop, subtract):
     ''' sums from start to stop (inclusive) and subtracts subtract
     start, stop, subtract, and output given in thousandths of an inch (no fractions)
     '''
+    assert stop > start
     out_value = 0
     for i in xrange(start, stop + 1):
         out_value += i
