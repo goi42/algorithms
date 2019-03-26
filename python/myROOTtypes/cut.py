73e0a1ceb6842d4e0b3157c9cb84d78835d03617
Michael Wilkinson <miwilkin@syr.edu>
2019-03-26 09:52:58 -0400

    cut: arithmetic now handles all parameters

--- python/myROOTtypes/cut.py
+++ python/myROOTtypes/cut.py
@@ -15,42 +15,104 @@ class cut(cbfch):
             ancut = another.cut.GetTitle()
             anname = another.name
             anweight = another.weight
+            anlinecolor = another.linecolor
+            anfillcolor = another.fillcolor
+            anfillstyle = another.fillstyle
+            anhname = another.hname
+            anneededbranchnames = another.neededbranchnames
+            anevaltemp = another.evaltemp
+            anneedednames = another.needednames
         elif another.__class__.__name__ == 'str':
             ancut = another
             anname = another
-            anweight = None
+            anweight = anlinecolor = anfillcolor = anfillstyle = anhname = anneededbranchnames = anevaltemp = anneedednames = None
         elif another.__class__.__name__ == 'TCut':
             ancut = another.GetTitle()
             anname = another.GetName()
-            anweight = None
+            anweight = anlinecolor = anfillcolor = anfillstyle = anhname = anneededbranchnames = anevaltemp = anneedednames = None
         else:
             raise TypeError('cannot perform arithmetic on object of class "' + another.__class__.__name__ + '" with object of class "cut"')
         
         newcut = logical_combine(self.cut.GetTitle(), sym, ancut)
         newname = logical_combine(self.name, sym, anname)
-        newhname = self.hname + altsym + another.hname if (another.__class__.__name__ == self.__class__.__name__ and (self.hname is not None and another.hname is not None)) else None
         if self.weight == anweight:
             newweight = self.weight
         else:
             raise Exception('cannot combine two cuts with different weights!')
+        newlinecolor = self.linecolor if self.linecolor == anlinecolor else None
+        newfillcolor = self.fillcolor if self.fillcolor == anfillcolor else None
+        newfillstyle = self.fillstyle if self.fillstyle == anfillstyle else None
+        newhname = self.hname + altsym + anhname if (self.hname is not None and anhname is not None) else None
+        newneededbranchnames = (
+            self.neededbranchnames + anneededbranchnames
+        ) if (
+            self.neededbranchnames is not None and anneededbranchnames is not None
+        ) else self.neededbranchnames if self.neededbranchnames is not None else anneededbranchnames
+        newevaltemp = logical_combine(
+            self.evaltemp, sym, anevaltemp
+        ) if (
+            self.evaltemp is not None and anevaltemp is not None
+        ) else None
+        newneedednames = (
+            self.needednames + anneedednames
+        ) if (
+            self.needednames is not None and anneedednames is not None
+        ) else self.needednames if self.needednames is not None else anneedednames
         
-        return newcut, newname, ancut, anname, newhname, newweight
+        return (
+            newcut, newname,
+            ancut, anname,
+            newweight,
+            newlinecolor, newfillcolor, newfillstyle,
+            newhname,
+            newneededbranchnames,
+            newevaltemp,
+            newneedednames,
+        )
     
     def __add__(self, another):
-        newcut, newname, ancut, anname, newhname, newweight = self._arithmetic('&&', another, '_and_')
+        (
+            newcut, newname,
+            ancut, anname,
+            newweight,
+            newlinecolor, newfillcolor, newfillstyle,
+            newhname,
+            newneededbranchnames,
+            newevaltemp,
+            newneedednames,
+        ) = self._arithmetic('&&', another, '_and_')
         if not self.cut.GetTitle().strip():
             newcut = ancut
             if not self.name.strip():
                 newname = anname
-        return cut(newcut, newname, hname=newhname, weight=newweight)
+        return cut(
+            newcut, newname,
+            weight=newweight, hname=newhname, evaltemp=newevaltemp,
+            linecolor=newlinecolor, fillcolor=newfillcolor, fillstyle=newfillstyle,
+            neededbranchnames=newneededbranchnames, needednames=newneedednames,
+        )
     
     def __sub__(self, another):
-        newcut, newname, ancut, anname, newhname, newweight = self._arithmetic('&& !', another, '_and_not_')
+        (
+            newcut, newname,
+            ancut, anname,
+            newweight,
+            newlinecolor, newfillcolor, newfillstyle,
+            newhname,
+            newneededbranchnames,
+            newevaltemp,
+            newneedednames,
+        ) = self._arithmetic('&& !', another, '_and_not_')
         if not self.cut.GetTitle().strip():
             newcut = '!(' + ancut + ')'
             if not self.name.strip():
                 newname = '!(' + anname + ')'
-        return cut(newcut, newname, hname=newhname, weight=newweight)
+        return cut(
+            newcut, newname,
+            weight=newweight, hname=newhname, evaltemp=newevaltemp,
+            linecolor=newlinecolor, fillcolor=newfillcolor, fillstyle=newfillstyle,
+            neededbranchnames=newneededbranchnames, needednames=newneedednames,
+        )
     
     def __neg__(self):
         return cut('') - self
@@ -58,22 +120,50 @@ class cut(cbfch):
     def __mul__(self, another):
         '''useful for combining weights
         '''
-        newcut, newname, ancut, anname, newhname, newweight = self._arithmetic('*', another, '_times_')
+        (
+            newcut, newname,
+            ancut, anname,
+            newweight,
+            newlinecolor, newfillcolor, newfillstyle,
+            newhname,
+            newneededbranchnames,
+            newevaltemp,
+            newneedednames,
+        ) = self._arithmetic('*', another, '_times_')
         if not self.cut.GetTitle().strip():
             newcut = ancut
             if not self.name.strip():
                 newname = anname
-        return cut(newcut, newname, hname=newhname, weight=newweight)
+        return cut(
+            newcut, newname,
+            weight=newweight, hname=newhname, evaltemp=newevaltemp,
+            linecolor=newlinecolor, fillcolor=newfillcolor, fillstyle=newfillstyle,
+            neededbranchnames=newneededbranchnames, needednames=newneedednames,
+        )
     
     def __div__(self, another):
         '''not actual division--a wonky way to do OR
         '''
-        newcut, newname, ancut, anname, newhname, newweight = self._arithmetic('||', another, '_or_')
+        (
+            newcut, newname,
+            ancut, anname,
+            newweight,
+            newlinecolor, newfillcolor, newfillstyle,
+            newhname,
+            newneededbranchnames,
+            newevaltemp,
+            newneedednames,
+        ) = self._arithmetic('||', another, '_or_')
         if not self.cut.GetTitle().strip():
             newcut = ancut
             if not self.name.strip():
                 newname = anname
-        return cut(newcut, newname, hname=newhname, weight=newweight)
+        return cut(
+            newcut, newname,
+            weight=newweight, hname=newhname, evaltemp=newevaltemp,
+            linecolor=newlinecolor, fillcolor=newfillcolor, fillstyle=newfillstyle,
+            neededbranchnames=newneededbranchnames, needednames=newneedednames,
+        )
     
     def __iadd__(self, another):
         return self + another
